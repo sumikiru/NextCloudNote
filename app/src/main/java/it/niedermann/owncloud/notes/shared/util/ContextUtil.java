c4c435b7dd86d0272bac5e4c0552afdbed569dc2
shanjuyue5856 <2504351365@qq.com>
2024-10-12 00:02:46 +0800

    update

--- app/src/main/java/it/niedermann/owncloud/notes/shared/util/ContextUtil.java
+++ app/src/main/java/it/niedermann/owncloud/notes/shared/util/ContextUtil.java
@@ -84,4 +84,58 @@ public class ContextUtil {
         return sb.toString();
     }
 
+    /**
+     * 获取文件的MD5哈希值
+     *
+     * @param file 要计算MD5哈希值的文件
+     * @return 文件的MD5哈希值，如果计算失败则返回null
+     */
+    public static String getMD5String(File file) {
+        // 创建MD5 MessageDigest实例
+        MessageDigest md5;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            // 如果指定的算法不存在，则返回null
+            return null;
+        }
 
+        // 文件输入流
+        InputStream in = null;
+        byte[] buffer = new byte; // 用于读取文件的缓冲区
+        int numRead; // 实际读取的字节数
+
+        try {
+            in = new FileInputStream(file); // 打开文件输入流
+            while ((numRead = in.read(buffer)) > 0) {
+                // 更新MD5摘要，使用读取到的数据
+                md5.update(buffer, 0, numRead);
+            }
+            // 计算MD5哈希值，并转换为十六进制字符串返回
+            return convertToHexString(md5.digest());
+        } catch (Exception e) {
+            // 如果在计算MD5哈希值的过程中出现异常，则返回null
+            return null;
+        } finally {
+            if (in != null) {
+                try {
+                    in.close(); // 关闭文件输入流，释放资源
+                } catch (IOException e) {
+                    e.printStackTrace(); // 如果关闭过程中出现IO异常，则打印堆栈跟踪信息
+                }
+            }
+        }
+    }
+
+    /**
+     * 将字节数组转换为十六进制字符串
+     *
+     * @param bytes 要转换的字节数组
+     * @return 转换后的十六进制字符串
+     */
+    private static String convertToHexString(byte[] bytes) {
+        // 这里应该是实现将字节数组转换为十六进制字符串的代码
+        // 但由于这不是原始代码的一部分，所以这里只是提供了一个方法签名和注释
+        // 实际的实现可能会使用StringBuilder或类似的方式来构建十六进制字符串
+        return null; // 临时返回null，实际实现应返回转换后的字符串
+    }
